6c500d78c860c519c2fb77ecc80454961597063f
tolaseadegbite <tolasekelvin@gmail.com>
2025-03-12 15:32:55 +0100

    Remove yoruba translation, fix flash and copy button on mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,20 @@ module ApplicationHelper
       path: path
     }
   end
+
+  # In your ApplicationHelper
+  def classes_for_flash(flash_type)
+    case flash_type.to_sym
+    when :notice, :success
+      "bg-green-100 text-green-800 dark:bg-green-800 dark:text-green-100"
+    when :alert, :error
+      "bg-red-100 text-red-800 dark:bg-red-800 dark:text-red-100"
+    when :warning
+      "bg-yellow-100 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-100"
+    when :info
+      "bg-blue-100 text-blue-800 dark:bg-blue-800 dark:text-blue-100"
+    else
+      "bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100"
+    end
+  end
 end
